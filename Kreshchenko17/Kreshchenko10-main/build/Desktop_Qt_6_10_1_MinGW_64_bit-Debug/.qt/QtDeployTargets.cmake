5466cc948460738256567e5b6f0a8b8402b3b672
Vash Nickname <vash_email@example.com>
2026-04-17 18:09:21 +0300

    Fix repo structure - remove nested git

--- Kreshchenko17/Kreshchenko10-main/build/Desktop_Qt_6_10_1_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_Kreshchenko10_FILE F:/qtdz/Kreshchenko10/build/Desktop_Qt_6_10_1_MinGW_64_bit-Debug/Kreshchenko10.exe)
-set(__QT_DEPLOY_TARGET_Kreshchenko10_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_Kreshchenko10_RUNTIME_DLLS F:/QL/6.10.1/mingw_64/bin/Qt6Widgets.dll;F:/QL/6.10.1/mingw_64/bin/Qt6Gui.dll;F:/QL/6.10.1/mingw_64/bin/Qt6Core.dll)
